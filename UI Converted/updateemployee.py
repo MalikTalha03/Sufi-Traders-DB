2b4f791008af32c75ccb8b19f46fbffc7e272c99
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-02 04:57:56 +0500

    Improved data retrieval

--- UI Converted/updateemployee.py
+++ UI Converted/updateemployee.py
@@ -8,15 +8,10 @@
 
 from PyQt6 import QtCore, QtGui, QtWidgets
 import pyodbc
-
+from db import DatabaseManager
 class Ui_Form(object):
     def __init__(self):
-        self.cnxn_str = (
-            "Driver={SQL Server};"
-            "Server=MALIK-TALHA;"
-            "Database=Sufi_Traders;"
-            "Trusted_Connection=yes;"
-        )
+        self.db = DatabaseManager()
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(371, 342)
@@ -77,15 +72,9 @@ class Ui_Form(object):
         self.pushButton.setText(_translate("Form", "Update"))
         self.label_7.setText(_translate("Form", "Employee ID"))
     def findemp(self):
-        cnxn = None
-        try:
-            cnxn = pyodbc.connect(self.cnxn_str)
-            cursor = cnxn.cursor()
-            cursor.execute(
-                "SELECT * FROM Employee WHERE employeeID = ?", self.lineEdit_6.text()
-            )
-            row = cursor.fetchone()
-            if row:
+        rows = self.db.execute_read_query("SELECT * FROM Employee WHERE employeeID = '{}'".format(self.lineEdit_6.text()))
+        if rows:
+            for row in rows:
                 self.lineEdit.setText(row[1])
                 self.lineEdit_3.setText(row[2])
                 self.lineEdit_2.setText(row[3])
@@ -94,62 +83,23 @@ class Ui_Form(object):
                 self.lineEdit_6.setEnabled(False)
                 self.lineEdit.setFocus()
                 self.pushButton.clicked.connect(self.updateemp)
-            else:
-                msg_box = QtWidgets.QMessageBox()
-                msg_box.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-                msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
-                msg_box.setText("Employee not found!")
-                msg_box.setWindowTitle("Error")
-                msg_box.exec()
-        except pyodbc.Error as err:
+        else:
             msg_box = QtWidgets.QMessageBox()
             msg_box.setIcon(QtWidgets.QMessageBox.Icon.Warning)
             msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
-            msg_box.setText('{}'.format(err))
+            msg_box.setText("Employee not found!")
             msg_box.setWindowTitle("Error")
             msg_box.exec()
-        finally:
-            if cnxn:
-                cnxn.close()
+
     def updateemp(self):
-        cnxn = None
-        try:
-            cnxn = pyodbc.connect(self.cnxn_str)
-            cursor = cnxn.cursor()
-            cursor.execute(
-                "UPDATE Employee SET empFName = ?, empLName = ?, employeeAddress = ?, empContact = ?, salary = ? WHERE employeeID = ?",
-                self.lineEdit.text(),
-                self.lineEdit_3.text(),
-                self.lineEdit_2.text(),
-                self.lineEdit_4.text(),
-                self.lineEdit_5.text(),
-                self.lineEdit_6.text(),
-            )
-            cnxn.commit()
-            msg_box = QtWidgets.QMessageBox()
-            msg_box.setIcon(QtWidgets.QMessageBox.Icon.Information)
-            msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
-            msg_box.setText("Employee updated successfully!")
-            msg_box.setWindowTitle("Success")
-            msg_box.exec()
-            self.lineEdit_6.setEnabled(True)
-            self.lineEdit_6.setFocus()
-            self.lineEdit.setText("")
-            self.lineEdit_3.setText("")
-            self.lineEdit_2.setText("")
-            self.lineEdit_4.setText("")
-            self.lineEdit_5.setText("")
-            self.pushButton.disconnect()
-        except pyodbc.Error as err:
-            msg_box = QtWidgets.QMessageBox()
-            msg_box.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-            msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
-            msg_box.setText('{}'.format(err))
-            msg_box.setWindowTitle("Error")
-            msg_box.exec()
-        finally:
-            if cnxn:
-                cnxn.close()
+        self.db.execute_query("UPDATE Employee SET empFName = '{}', empLName = '{}', employeeAddress = '{}', empContact = '{}', salary = '{}' WHERE employeeID = '{}'".format(self.lineEdit.text(), self.lineEdit_3.text(), self.lineEdit_2.text(), self.lineEdit_4.text(), self.lineEdit_5.text(), self.lineEdit_6.text()))
+        msg_box = QtWidgets.QMessageBox()
+        msg_box.setIcon(QtWidgets.QMessageBox.Icon.Information)
+        msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
+        msg_box.setText("Employee updated successfully!")
+        msg_box.setWindowTitle("Success")
+        msg_box.exec()
+        Form.close()
 
 
 if __name__ == "__main__":
