c524a050614e256d9d3e523f185277c8b43a904a
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-07 15:00:13 +0500

    Update window title in updatecustomer.py

--- UI Converted/updatecustomer.py
+++ UI Converted/updatecustomer.py
@@ -56,7 +56,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Update Customer Information"))
         self.label.setText(_translate("Form", "Customer ID"))
         self.label_2.setText(_translate("Form", "First Name"))
         self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
