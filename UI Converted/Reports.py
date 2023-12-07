0031ae39157ef5c62646cc05fc96bb91e7aa2b1d
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-07 14:59:04 +0500

    Update window title to "Sales Report"

--- UI Converted/Reports.py
+++ UI Converted/Reports.py
@@ -74,7 +74,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Sales Report"))
         self.label.setText(_translate("MainWindow", "Sales Report"))
         self.label_2.setText(_translate("MainWindow", "Start Date"))
         self.label_3.setText(_translate("MainWindow", "End Date"))
