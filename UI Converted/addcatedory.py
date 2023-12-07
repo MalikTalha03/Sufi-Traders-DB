e83a6faf5a98f089130df75981a71d77ca9e43cf
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-07 14:51:31 +0500

    Update window title in addcatedory.py

--- UI Converted/addcatedory.py
+++ UI Converted/addcatedory.py
@@ -39,7 +39,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Add Category"))
         self.label.setText(_translate("Form", "Category ID"))
         self.pushButton.setText(_translate("Form", "ADD"))
         self.label_2.setText(_translate("Form", "Category Name"))
