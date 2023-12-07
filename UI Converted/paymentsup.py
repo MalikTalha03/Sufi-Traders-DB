4646b3a03e48f09800041fe44344c3814e00c067
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-07 14:58:41 +0500

    Update window title to "Payment"

--- UI Converted/paymentsup.py
+++ UI Converted/paymentsup.py
@@ -61,7 +61,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Payment"))
         self.label_8.setText(_translate("Form", "Total"))
         self.label_7.setText(_translate("Form", "Order ID"))
         self.label.setText(_translate("Form", "Payment Method"))
