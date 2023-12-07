8640319b6c53e1285bf85b5fbdedb717c5ffec33
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-07 15:00:29 +0500

    Update window title in updsupplier.py

--- UI Converted/updsupplier.py
+++ UI Converted/updsupplier.py
@@ -49,7 +49,7 @@ class Ui_Form(object):
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "Update Supplier Details"))
         self.textBrowser.setHtml(_translate("Form", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
