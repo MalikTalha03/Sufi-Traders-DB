1eb0a668225e61bf848b801cb268f24eae364685
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-17 12:55:38 +0500

    Refactor sales query to calculate monthly sales

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,15 +1,17 @@
 SELECT
-    CONVERT(DATE, CO.orderDate) AS OrderDate,
-    ISNULL(SUM(COD.quantity * COD.salePrice), 0) AS DailySales
+    YEAR(CO.orderDate) AS OrderYear,
+    MONTH(CO.orderDate) AS OrderMonth,
+    ISNULL(SUM(COD.quantity * COD.salePrice), 0) AS MonthlySales
 FROM
     Customer_Order CO
 LEFT JOIN
     Customer_Order_Details COD ON CO.orderID = COD.orderID
 WHERE
     CO.customerID = 1
-    AND MONTH(CO.orderDate) = MONTH(GETDATE())
     AND YEAR(CO.orderDate) = YEAR(GETDATE())
 GROUP BY
-    CONVERT(DATE, CO.orderDate)
+    YEAR(CO.orderDate),
+    MONTH(CO.orderDate)
 ORDER BY
-    OrderDate;
+    OrderYear,
+    OrderMonth;
