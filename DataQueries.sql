5f2df64bf6ca23ec15ffeecd2f988759aaa05087
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-21 10:48:21 +0500

    Refactor SQL query to calculate hourly sales

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,17 +1,13 @@
 SELECT
-    P.productID,
-    P.productName,
-    ISNULL(SUM(COD.quantity * COD.salePrice), 0) AS DailySales
-FROM
-    Customer_Order CO
-JOIN
-    Customer_Order_Details COD ON CO.orderID = COD.orderID
-JOIN
-    Products P ON COD.productID = P.productID
-WHERE
-    CONVERT(DATE, CO.orderDate) = GETDATE()
-GROUP BY
-    P.productID,
-    P.productName
-ORDER BY
-    DailySales DESC;
+                                    DATEPART(HOUR, CO.orderTime) AS OrderHour,
+                                    SUM(COD.quantity * COD.salePrice) AS HourlySales
+                                FROM
+                                    Customer_Order CO
+                                JOIN
+                                    Customer_Order_Details COD ON CO.orderID = COD.orderID
+                                WHERE
+                                    CONVERT(DATE, CO.orderDate) = 'GETDATE()'
+                                GROUP BY
+                                    DATEPART(HOUR, CO.orderTime)
+                                ORDER BY
+                                    OrderHour;
