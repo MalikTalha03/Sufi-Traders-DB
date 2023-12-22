cf81fcc7c39748db2c48685f865d0d4f9af08496
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-22 22:41:38 +0500

    Refactor data query to calculate hourly sales

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,11 +1,13 @@
-SELECT DISTINCT
-                            Employee.employeeID,
-                            Employee.empFName,
-                            Employee.empLName
-                        FROM
-                            Customer_Order CO
-                        JOIN
-                            Employee ON CO.employeeID = Employee.employeeID
-                        WHERE
-                            MONTH(CONVERT(DATE, CO.orderDate)) = MONTH(GETDATE())
-                                AND YEAR(CONVERT(DATE, CO.orderDate)) = YEAR(GETDATE());
+SELECT
+                                        DATEPART(HOUR, CO.orderTime) AS OrderHour,
+                                        ISNULL(SUM(COD.quantity * COD.salePrice), 0) AS HourlySales
+                                    FROM
+                                        Customer_Order CO
+                                    LEFT JOIN
+                                        Customer_Order_Details COD ON CO.orderID = COD.orderID
+                                    WHERE
+                                        COD.productID = 1 AND CONVERT(DATE, CO.orderDate) = GETDATE()
+                                    GROUP BY
+                                        DATEPART(HOUR, CO.orderTime)
+                                    ORDER BY
+                                        OrderHour;
