60a328fcbe2db581dfa7d472a8d2dd2a28b8922e
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-23 21:43:19 +0500

    Refactor daily sales query to use Customer_Transactions table

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,14 +1,1 @@
-SELECT
-                                            DAY(CO.orderDate) AS OrderDay,
-                                            ISNULL(SUM(COD.quantity * COD.salePrice), 0) AS DailySales
-                                        FROM
-                                            Customer_Order CO
-                                        LEFT JOIN
-                                            Customer_Order_Details COD ON CO.orderID = COD.orderID
-                                        WHERE
-                                            COD.productID = 1 AND MONTH(CONVERT(DATE, CO.orderDate)) = MONTH(GETDATE())
-                                                AND YEAR(CONVERT(DATE, CO.orderDate)) = YEAR(GETDATE())
-                                        GROUP BY
-                                            DAY(CO.orderDate)
-                                        ORDER BY
-                                            OrderDay;
+SELECT SUM(totalAmount) AS totalsales FROM Customer_Transactions WHERE transactionDate = 'GETDATE()'
