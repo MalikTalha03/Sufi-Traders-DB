04121c8f6d8338ac88388398687789149816332d
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-24 21:16:40 +0500

    Add query to retrieve employee username for active session

--- DataQueries.sql
+++ DataQueries.sql
@@ -1,1 +1,5 @@
-SELECT SUM(totalAmount) AS totalsales FROM Customer_Transactions WHERE transactionDate = 'GETDATE()'
+-- Assuming you want to retrieve the username for an active session
+SELECT E.empFName + ' ' + ISNULL(E.empLName, '') AS EmployeeUsername
+FROM Employee_Session ES
+JOIN Employee E ON ES.empID = E.employeeID
+WHERE ES.currStatus = 'Active';
