eae95015eadff6eb249495fad8c62da52ef6a27d
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-12-24 11:07:27 +0500

    Update Employee_Session table data types

--- Queries.sql
+++ Queries.sql
@@ -108,9 +108,9 @@ CREATE TABLE Supplier_Transactions (
 CREATE TABLE Employee_Session (
     sessionID int PRIMARY KEY,
     empID int FOREIGN KEY REFERENCES Employee(employeeID) NOT NULL,
-    loginTime datetime NOT NULL,
-    lastActivityTime datetime NOT NULL,
-    logoutTime datetime,
+    loginTime DATETIME2 NOT NULL,
+    lastActivityTime DATETIME2 NOT NULL,
+    logoutTime DATETIME2,
     currStatus varchar(15) NOT NULL,
     timeoutMinutes int NOT NULL,
     CONSTRAINT status CHECK (currStatus IN ('Active', 'Inactive'))
